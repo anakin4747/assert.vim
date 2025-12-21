49335d47b0bb22f9b4c7bd711d0d4df07986002e
Anakin Childerhose <anakin@childerhose.ca>
2025-12-21 16:57:04 -0500

    add nix flake and lock

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "Development environment for assert.vim";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                neovim
+                vim
+            ];
+        };
+    };
+}
